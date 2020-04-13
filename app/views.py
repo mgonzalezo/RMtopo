41c393eb6c814bbc8324a96f0493830bee22bd7f
marco <xmarcox1441@hotmail.com>
2020-04-13 11:26:17 +0900

    Changes performed

--- app/views.py
+++ app/views.py
@@ -5,10 +5,10 @@ from flask import render_template
 def index():
     return render_template("public/index.html")
 
-@app.route("/about")
-def about():
-    return "All about Flask"
-
 #@app.route("/admin/dashboard")
 #def admin_dashboard():
 #    return render_template("admin/dashboard.html")
+
+@app.route("/about")
+def about():
+    return "All about Flask"
