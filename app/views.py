75dae4c22176d97c2665d805f0e7e9f9a41983c7
marco <xmarcox1441@hotmail.com>
2020-04-13 20:41:34 +0900

    changes to work

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, send_from_directory
+import os
+
+app.config["IMAGE_UPLOADS"] = "/Users/marco.gonzalezortiz/Documents/Rakuten_Mobile/Marco/Learning/rmtopo/app/static"
 
 @app.route("/")
 def index():
@@ -12,3 +15,17 @@ def index():
 @app.route("/about")
 def about():
     return "All about Flask"
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+            print("Image saved")
+            return redirect(request.url)
+    return render_template("public/core.html")
+
+#@app.route('/upload-image/<filename>')
+#def send_image(filename):
+#    return send_from_directory('images',filename)
