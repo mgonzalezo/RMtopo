89c1b619c438d679bc2738196503ac7f0a15cd1d
Marco <marco@entel.pe>
2020-04-26 19:56:18 +0900

    config db

--- config.py
+++ config.py
@@ -1,14 +1,26 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+class Config:
+    """Set Flask configuration vars from .env file."""
 
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'key123'
-    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    #SQLALCHEMY_DATABASE_URI = 
+    # General Config
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+
+# class Config(object):
+#     DEBUG = False
+#     TESTING = False
+#     CSRF_ENABLED = True
+#     SECRET_KEY = 'key123'
+#     #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+#     #SQLALCHEMY_DATABASE_URI = 
 
 
 class ProductionConfig(Config):
